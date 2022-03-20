24757fc0bf84b2f4f65383087460303fbfb82d9e
Manuel Odendahl <wesen@ruinwesen.com>
2022-03-20 11:56:53 -0400

    :sparkles: Add debug logging, console output, stderr logging, local logfile

--- cmd/raza/main.go
+++ cmd/raza/main.go
@@ -1,35 +1,54 @@
 package main
 
 import (
+	"github.com/mattn/go-isatty"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/wesen/raza/cmd/raza/query"
 	"github.com/wesen/raza/cmd/raza/server"
 	"github.com/wesen/raza/cmd/raza/shell"
 	"github.com/wesen/raza/cmd/raza/user"
+	"os"
+	"time"
 )
 
 var rootCmd = cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+		file := viper.GetString("root.log-file")
+		if file == "" {
+			if isatty.IsTerminal(os.Stderr.Fd()) {
+				log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+			} else {
+				log.Logger = log.Output(os.Stderr)
+			}
+		} else {
+			w, err := os.Open(file)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("Could not open log file %s", file)
+			}
+			log.Logger = log.Output(w)
+		}
+
+		if viper.GetBool("root.log-line") {
+			log.Logger = log.With().Caller().Logger()
+		}
+
 		log.Info().Msg("Starting raza...")
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
 		if viper.GetBool("root.debug") {
 			zerolog.SetGlobalLevel(zerolog.DebugLevel)
 		}
-
-		log.Debug().
-			Str("Scale", "833 cents").
-			Float64("Interval", 833.09).
-			Msg("Fibonnnnnaccciiii")
-		log.Debug().Str("Name", "Tom").
-			Send()
-		log.Print("hello world")
-
+		if viper.GetBool("root.log-error-stacktrace") {
+			log.Info().Msg("Logging error stacktraces")
+			zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+		}
 	},
 }
 
@@ -51,13 +70,17 @@ func main() {
 	viper.SetConfigName("raza")
 	viper.AddConfigPath("$HOME/.config")
 	if err := viper.ReadInConfig(); err != nil {
-		log.Error().Str("error", err.Error()).Msg("Failed to read config")
+		log.Error().Err(err).Msg("Failed to read config")
 	}
 
 	rootCmd.PersistentFlags().String("address", defaultRazaAddress, "The address of the raza server")
 	rootCmd.PersistentFlags().Bool("debug", false, "Enable debug logging")
-	if err := viperBindNestedPFlags("root", &rootCmd, []string{"address", "debug"}); err != nil {
-		log.Fatal().Str("error", err.Error()).Msg("Could not bind persistent flags")
+	rootCmd.PersistentFlags().Bool("log-error-stacktrace", false, "Enable stacktrace logging on errors")
+	rootCmd.PersistentFlags().Bool("log-line", false, "Enable logging of file ane line number")
+	rootCmd.PersistentFlags().String("log-file", "", "Enable logging to file")
+	if err := viperBindNestedPFlags("root", &rootCmd,
+		[]string{"address", "debug", "log-error-stacktrace", "log-line", "log-file"}); err != nil {
+		log.Fatal().Err(err).Msg("Could not bind persistent flags")
 	}
 
 	rootCmd.AddCommand(&shell.HookCmd)
